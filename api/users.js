855af23e337ec0313ede95bf636d2cac8cf649ef
Ezehuche <ezehuche4@gmail.com>
2020-12-12 17:34:01 +0100

    User authentication

--- api/users.js
+++ api/users.js
@@ -7,3 +7,20 @@ let path = require("path");
 let mkdirp = require("mkdirp");
 let bcrypt = require("bcryptjs");
 let Role = require("../models/role");
+
+
+module.exports = function (router, passport) {
+    router.get('/invitation/:invitation_id', function (req, res, next) {
+        Invitation.findOne("token", req.params.invitation_id, function (result) {
+            if (result.data) {
+                return res.json({"status": "valid token"});
+            } else {
+                return res.status(404).json({status: "bad token"});
+            }
+        });
+    });
+
+    require("./entity")(router, User, "users");
+
+    return router;
+};
--- config/init.js
+++ config/init.js
@@ -337,6 +337,23 @@ module.exports = function (initConfig) {
                     "developer": []
                 };
                 let permissions = [];
+                for (let route in swaggerJSON) {
+                    for (let method in swaggerJSON[route]) {
+                        if (swaggerJSON[route][method]['x-roles'].includes("admin")) {
+                            initialRoleMap.admin.push(swaggerJSON[route][method].operationId);
+                        }
+                        if (swaggerJSON[route][method]['x-roles'].includes("staff")) {
+                            initialRoleMap.staff.push(swaggerJSON[route][method].operationId);
+                        }
+                        if (swaggerJSON[route][method]['x-roles'].includes("developer")) {
+                            initialRoleMap.user.push(swaggerJSON[route][method].operationId);
+                        }
+                        if (swaggerJSON[route][method]['x-roles'].includes("user")) {
+                            initialRoleMap.user.push(swaggerJSON[route][method].operationId);
+                        }
+                        permissions.push(swaggerJSON[route][method].operationId);
+                    }
+                }
 
                 let roles = Object.keys(initialRoleMap);
 
