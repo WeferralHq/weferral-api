669a893452ff4b9b34bdb75904b5fe8bc486a5e3
Ezehuche <ezehuche4@gmail.com>
2021-02-25 20:18:44 +0100

    updated data analytics api

--- app.js
+++ app.js
@@ -24,9 +24,13 @@ module.exports = function (initConfig = null) {
             app.use(helmet({
                 frameguard: false
             }));
-            //var subpath = express();
 
-            /*app.use(function(req, res, next) {
+            app.use(logger('dev'));
+            app.use(bodyParser.json());
+            app.use(bodyParser.urlencoded({
+                extended: false
+            }));
+            app.use(function(req, res, next) {
                 res.header("Access-Control-Allow-Origin", "http://localhost:4100");
                 res.header("Access-Control-Allow-Credentials", true);
                 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept, X-Custom-Header");
@@ -35,24 +39,14 @@ module.exports = function (initConfig = null) {
                     return res.status(200).end();
                 }
                 next();
-            });*/
-
-
-            // uncomment after placing your favicon in /public
-            //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-            app.use(logger('dev'));
-            app.use(bodyParser.json());
-            app.use(bodyParser.urlencoded({
-                extended: false
-            }));
-            app.use(function(req, res, next) {
+            });
+            /*app.use(function(req, res, next) {
                 res.header("Access-Control-Allow-Credentials", true);
                 res.header("Access-Control-Allow-Origin", "*");
                 res.header("Access-Control-Allow-Methods", "*");
                 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
                 next();
-            });
+            });*/
             app.use(cookieParser());
 
             // TODO - Why Do we need this key ?
